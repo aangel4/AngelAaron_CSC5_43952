a72f674289c9643bf842e202d51cf396b550bbd9
aangel4 <aangel4@student.rcc.edu>
2015-03-18 11:57:24 -0700

    Signed-off-by: aangel4 <aangel4@student.rcc.edu>

--- Hmwk/Assignment 2/Gaddis_8thEd_Chap3_Prob8/main.cpp
+++ Hmwk/Assignment 2/Gaddis_8thEd_Chap3_Prob8/main.cpp
@@ -1,18 +1,43 @@
 /* 
  * File:   main.cpp
- * Author: Aaron
+ * Author: Aaron Angel
  *
  * Created on March 17, 2015, 11:57 PM
+ *      //Purpose: How Many Widgets Problem
  */
+//System Libraries
 
-#include <cstdlib>
+#include <iostream>
+#include <iomanip>
 
 using namespace std;
 
-/*
- * 
- */
+//User Libraries
+
+//Global Constants
+
+//Function Prototypes
+
+//Execution Begins Here!
+
 int main(int argc, char** argv) {
+    //Declare Variables
+    float pallet;//Pallet weight
+    float wigtlbs=12.5;//widget weight in pounds
+    float Pwidget;//total pallet and widget weight
+    unsigned short widget;// amount of widgets
+    //Prompt the user for inputs
+    cout<<"Input Pallet Weight"<<endl;
+    cin>>pallet;
+    cout<<"Input Amount of Widgets"<<endl;
+    cin>>widget;
+    //Calculations
+    Pwidget=pallet+(wigtlbs*widget);
+    
+    //Output Results
+    cout<<setprecision(2)<<fixed;
+    cout<<"Total Pallet and Widget Weight "<<Pwidget<<"Lbs"<<endl;
+    
 
     return 0;
 }
