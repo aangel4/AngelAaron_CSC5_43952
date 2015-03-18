d620d93687d4bcb5ee790ff0458d8e29c525450c
aangel4 <aangel4@student.rcc.edu>
2015-03-17 23:56:13 -0700

    Signed-off-by: aangel4 <aangel4@student.rcc.edu>

--- Hmwk/Assignment 2/Gaddis_8thEd_Chap3_Prob7/main.cpp
+++ Hmwk/Assignment 2/Gaddis_8thEd_Chap3_Prob7/main.cpp
@@ -14,22 +14,23 @@ using namespace std;
 
 //User Libraries
 //Global Constants
+unsigned char CNVPCT=100;
 //Function Prototype
 //Execution Begins here!!
 int main(int argc, char** argv) {
     //Declare Variables
     string MovieN;//Movie name
-    float adult;//Total adults
-    float child;//Total child
-    float adultT=10.00;//Amount of adult Ticket
-    float childT=6.00;//Amount of child ticket
+    unsigned short adult;//Total adults
+    unsigned short child;//Total child
+    unsigned short adultT=10.00;//Amount of adult Ticket
+    unsigned short childT=6.00;//Amount of child ticket
     float GBO;//Gross box office
     float NBO;//Net box office
     float AmtPd;//Amount paid to distributor
     
     //Prompt the user for inputs
     cout<<"Input the name of the movie "<<endl;
-    cin>>MovieN;
+    getline(cin, MovieN);
     cout<<"Input the amount of adult tickets sold"<<endl;
     cin>>adult;
     cout<<"Input the amount of child tickets sold"<<endl;
@@ -37,18 +38,18 @@ int main(int argc, char** argv) {
     
     //Calculations
     GBO=(adult*adultT)+(child*childT);
-    NBO=GBO*.20;
+    NBO=(GBO)*(20/static_cast<float>(CNVPCT));
     AmtPd=GBO-NBO;
     
     //Output Results
     
-    cout<<"Movie Name"<<MovieN<<endl;
-    cout<<"Adult Tickets Sold: "<<adult<<endl;
-    cout<<"Child Tickets Sold: "<<child<<endl;
+    cout<<"Movie Name "<<right<<setw(30)<<MovieN<<endl;
+    cout<<"Adult Tickets Sold: "<<setw(15)<<adult<<endl;
+    cout<<"Child Tickets Sold: "<<setw(15)<<child<<endl;
     cout<<setprecision(2)<<fixed;
-    cout<<"Gross Box Office Profit: $"<<GBO<<endl;
-    cout<<"Net Box Office Profit: $"<<NBO<<endl;
-    cout<<"Amount Paid to Distributor: $"<<AmtPd<<endl;
+    cout<<"Gross Box Office Profit:"<<setw(7)<<"$"<<GBO<<endl;
+    cout<<"Net Box Office Profit:"<<setw(9)<<"$"<<setw(2)<<NBO<<endl;
+    cout<<"Amount Paid to Distributor: "<<setw(3)<<"$"<<AmtPd<<endl;
     return 0;
 }
 
