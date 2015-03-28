ca73e91e1c3f7713948af79d43cd3ebac43462c6
aangel4 <aangel4@student.rcc.edu>
2015-03-28 01:01:28 -0700

    Signed-off-by: aangel4 <aangel4@student.rcc.edu>

--- Hmwk/Assignment 3/Gaddis_8thEd_Chap4_Prob2/main.cpp
+++ Hmwk/Assignment 3/Gaddis_8thEd_Chap4_Prob2/main.cpp
@@ -1,19 +1,55 @@
 /* 
  * File:   main.cpp
- * Author: Aaron
+ * Author: Aaron Angel
  *
  * Created on March 27, 2015, 11:54 PM
+ *      Purpose: Roman Numeral Converter Problem
  */
 
-#include <cstdlib>
+#include <iostream>
+
+//System Libraries
 
 using namespace std;
 
-/*
- * 
- */
-int main(int argc, char** argv) {
+//User Libraries
 
+//Global Constants
+
+//Function Prototype
+
+//Execution Begins Here!!
+int main(int argc, char** argv) {
+//Declare Variables
+   unsigned short number; //Number for conversion to Roman numeral
+
+    //Prompt the user for inputs
+    cout<<"Input a number from 1-10 to convert to Roman Numeral "<<endl;
+    cin>>number;
+    switch (number)
+    {
+        case 1: cout<<" I "<<endl;
+                  break;
+        case 2: cout<<"II"<<endl;
+                  break;
+        case 3: cout<<"III"<<endl;
+                  break; 
+        case 4: cout<<"IV"<<endl;
+                  break;
+        case 5: cout<<" V"<<endl;
+                  break;
+        case 6: cout<<"VI"<<endl;
+                  break;
+        case 7: cout<<" VII"<<endl;
+                  break;
+        case 8: cout<<"VIII"<<endl;
+                  break;
+        case 9: cout<<"IX"<<endl;
+                  break;
+        case 10: cout<<"X"<<endl;
+                  break;
+        default: cout<<"You did not enter a number from 1-10"<<endl;
+    }
     return 0;
 }
 
