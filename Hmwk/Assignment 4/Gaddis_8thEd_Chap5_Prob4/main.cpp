5e3ac974afdc677da3b64f0fffac21a8f98a3207
aangel4 <aangel4@student.rcc.edu>
2015-04-06 16:51:40 -0700

    Signed-off-by: aangel4 <aangel4@student.rcc.edu>

---
+++ Hmwk/Assignment 4/Gaddis_8thEd_Chap5_Prob4/main.cpp
@@ -0,0 +1,35 @@
+/* 
+ * File:   main.cpp
+ * Author: Aaron Angel
+ * Purpose: Calories Burned Problem
+ * Created on April 6, 2015, 4:32 PM
+ */
+
+//System Libraries
+#include <iostream>
+
+using namespace std;
+
+//User Libraries
+
+//Global Constants
+
+//Function Prototypes
+
+//Execution Begins Here!!
+int main(int argc, char** argv) {
+//Declare Variables
+    float cal=3.6;//Calories burned per min
+    const char StrtMin=5;//Starting minutes
+    const unsigned char EndMin=30;//Ending minutes
+    unsigned short min;//Minutes
+//Table header
+    cout<<"Minutes Calories Burned "<<endl;
+    cout<<"------------------------"<<endl;
+//For loop
+    for(min=StrtMin;min<=EndMin;min+=5)
+        cout<<min<<"\t\t"<<(min*cal)<<endl;
+        
+    return 0;
+}
+
